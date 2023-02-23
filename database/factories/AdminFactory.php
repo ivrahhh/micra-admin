ead5a7b679b101c9c6a7fd662b185ef05fc4c98e
harvicapino22 <harvicapino22@gmail.com>
2023-02-23 22:28:21 +0800

    created Admin model

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
+ */
+class AdminFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'position' => 'Admin',
+            'contact' => fake()->unique()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => Hash::make('password'),
+        ];
+    }
+}
